6bcb6e8ba2d686aced4cfddea7412741d8035718
Milos Dzeletovic <miloshdz@gmail.com>
2018-06-25 10:39:21 +0200

    Add Poll to Store

--- src/components/AddPoll.js
+++ src/components/AddPoll.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddPoll } from '../actions/polls'
 
 class AddPoll extends Component {
   state = {
@@ -30,7 +32,7 @@ class AddPoll extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     // Redirect to /
-    console.log('Add poll:', this.state)
+    this.props.dispatch(handleAddPoll(this.state))
   }
   render() {
     const { question, a, b, c, d } = this.state
@@ -96,4 +98,4 @@ class AddPoll extends Component {
 }
 
 
-export default AddPoll
+export default connect()(AddPoll)
